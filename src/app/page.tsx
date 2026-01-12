7155839a425971dd870c8581de16a80fd2cae3e1
elvishgs <elvishuges@hotmail.com>
2026-01-11 22:27:32 -0300

    add: arquivos e post model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
+import { Header } from "@/components/Header";
 import clsx from "clsx";
 
 export default function Home() {
+  console.log("HOME");
   return (
-    <h1
-      className={clsx(
-        "text-6xl",
-        "font-bold",
-        "text-blue-500",
-        "hover:text-blue-50",
-        "hover:bg-blue-500"
-      )}
-    >
-      Texto no meu H1
-    </h1>
+    <div>
+      <Header />
+    </div>
   );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import clsx from "clsx";
+
+export function Header() {
+  console.log("Header");
+  return (
+    <h1
+      className={clsx(
+        "text-6xl",
+        "font-bold",
+        "text-blue-500",
+        "hover:text-blue-50",
+        "hover:bg-blue-500"
+      )}
+    >
+      Texto no meu H1
+    </h1>
+  );
+}
