9718b39033c4d015238ebb6a39d25eaaaeaef522
elvishgs <elvishuges@hotmail.com>
2026-01-12 23:51:29 -0300

    add: aplicando classes tailwind na image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
 import { Container } from "@/components/Container";
+import { Header } from "@/components/Header";
 import PostsList from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
+import Image from "next/image";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
     <Container>
-      <header>
-        <h1 className="text-6xl font-bold text-center py-8">Aqui é a HEADER</h1>
-      </header>
-
+      <Header />
+      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
+        <Link className="w-full h-full overflow-hidden rounded-xl" href={""}>
+          <Image
+            className="group-hover:scale-105 transition"
+            src="/images/bryen_0.png"
+            width={1200}
+            height={720}
+            alt="Post"
+          ></Image>
+        </Link>
+        <p>Lorem</p>
+      </section>
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
