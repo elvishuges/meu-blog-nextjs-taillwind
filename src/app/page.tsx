e5f6c211d6b298147ff1f151506531580d0c2878
elvishgs <elvishuges@hotmail.com>
2026-01-13 22:25:43 -0300

    commit: componente PostImageCover e PostHeading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,51 @@
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
+import { PostCoverImage } from "@/components/PostCoverImage";
+import { PostHeading } from "@/components/PostHeading";
 import PostsList from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
-import Image from "next/image";
-import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
     <Container>
       <Header />
+
       <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
-        <Link className="w-full h-full overflow-hidden rounded-xl" href={""}>
-          <Image
-            className="group-hover:scale-105 transition"
-            src="/images/bryen_0.png"
-            width={1200}
-            height={720}
-            alt="Post"
-          ></Image>
-        </Link>
-        <p>Lorem</p>
+        <PostCoverImage
+          linkProps={{
+            href: "/post/asdfasdf",
+          }}
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: "/images/bryen_9.png",
+            alt: "Alt da imagem",
+            priority: true,
+          }}
+        />
+
+        <div className="flex flex-col gap-4 sm:justify-center">
+          <time
+            className="text-slate-600 block text-sm/tight"
+            dateTime="2025-04-20"
+          >
+            20/04/2025 10:00
+          </time>
+
+          <PostHeading as="h1" url="#">
+            Rerum, vel ex? Impedit ullam harum blanditiis
+          </PostHeading>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
+            dolorem est dolor porro, doloribus neque, quidem mollitia
+            doloremque, ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam
+            harum blanditiis mollitia?
+          </p>
+        </div>
       </section>
+
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
