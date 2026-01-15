138b67c102536e07d2c4fcda5f6d5c4fd420e20b
elvishgs <elvishuges@hotmail.com>
2026-01-15 18:24:29 -0300

    refactor: criando componentes Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,13 @@ import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <Container>
-      <Header />
+    <>
       <Suspense fallback={<SpinLoader />}>
         <PostFeutered />
       </Suspense>
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
-
-      <footer>
-        <p className="text-6xl font-bold text-center py-8">Footer</p>
-      </footer>
-    </Container>
+    </>
   );
 }
